7bed5c1647465a23a9bf9d2cd58972a091c5b4fe
DAVID_Chloe <david.chloe@educ-valadon-limoges.fr>
2025-12-05 09:44:56 +0100

    home page

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/users/simple/')),
     path('admin/', admin.site.urls),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
